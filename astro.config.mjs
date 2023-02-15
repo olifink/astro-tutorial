594806ad6985d3900f537803d7f9d961faa2d09e
Oliver Fink <323250+olifink@users.noreply.github.com>
2023-02-15 20:34:17 +0100

    first without config base-site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // base: '/astro-tutorial',
+    // site: 'https://olifink.github.io/astro-tutorial'
+});

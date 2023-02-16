38189049cc155f972c07a9497db7cc02dbc1978b
Oliver Fink <323250+olifink@users.noreply.github.com>
2023-02-16 16:11:11 +0100

    now?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  site: 'https://olifink.github.io/',
+  site: 'https://olifink.github.io',
   base: '/astro-tutorial',
 }
 );
